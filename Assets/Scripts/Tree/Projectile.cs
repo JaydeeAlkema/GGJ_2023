150fb80faf41fe9ed314825b210b8492572c0635
joris d <j_d_derks@hotmail.com>
2023-02-05 12:16:43 +0100

    healthbars now update when leaves damage player

--- Assets/Scripts/Tree/Projectile.cs
+++ Assets/Scripts/Tree/Projectile.cs
@@ -28,5 +28,6 @@ public class Projectile : MonoBehaviour, IDamageable
         player.Health -= damage;
         Debug.Log(player.Health);
         Destroy(gameObject);
+        GameManager.instance.UiManager.UpdateHealthBars();
     }
 }
