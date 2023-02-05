684d1a5efedd3dc11d5c2f8f910ac73d560dd6e8
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-02-05 02:13:27 +0100

    Add attacks.

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour, IDamageable
+public class Attack : MonoBehaviour/*, IDamageable*/
 {
 	// TODO:
 	// 1. Use scriptable Int instead
 	[SerializeField] private int damage = 10;
+	[SerializeField] private float destroyTime = 0.5f;
 
 	private bool canDealDamage = true;
 
-	public void DealDamage(int damage, PlayerController player)
+	public GameObject sender = null;
+
+	private void OnEnable()
 	{
-		if (!canDealDamage) return;
-		player.Health -= damage;
-		canDealDamage = false;
+		Destroy(gameObject, destroyTime);
+	}
+
+	//public void DealDamage(int damage, PlayerController player)
+	//{
+	//	if (!canDealDamage) return;
+	//	player.Health -= damage;
+	//	canDealDamage = false;
+	//}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (!canDealDamage || collision.gameObject == sender) return;
+		if (collision.GetComponent<PlayerController>() != null) collision.GetComponent<PlayerController>().Health -= damage;
 	}
 }
